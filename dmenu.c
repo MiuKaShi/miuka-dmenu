9abbf5fb256ae1a751c6580b3c333df15ed7fb1a
miuka <d.lolimiuku@gmail.com>
2026-05-02 10:35:05 +0900

    fix shortcuts

--- dmenu.c
+++ dmenu.c
@@ -403,6 +403,7 @@ keypress(XKeyEvent *ev)
 		return;
 	case XLookupChars: /* composed string from input method */
 		goto insert;
+	case XLookupKeySym:
 	case XLookupBoth: /* a KeySym and a string are returned: use keysym */
 		break;
 	}
@@ -443,9 +444,11 @@ keypress(XKeyEvent *ev)
 			XConvertSelection(dpy, (ev->state & ShiftMask) ? clip : XA_PRIMARY,
 			                  utf8, utf8, win, CurrentTime);
 			return;
+		case XK_Left:
 		case XK_KP_Left:
 			movewordedge(-1);
 			goto draw;
+		case XK_Right:
 		case XK_KP_Right:
 			movewordedge(+1);
 			goto draw;
@@ -483,6 +486,7 @@ insert:
 		if (!iscntrl((unsigned char)*buf))
 			insert(buf, len);
 		break;
+	case XK_Delete:
 	case XK_KP_Delete:
 		if (text[cursor] == '\0')
 			return;
@@ -493,6 +497,7 @@ insert:
 			return;
 		insert(NULL, nextrune(-1) - cursor);
 		break;
+	case XK_End:
 	case XK_KP_End:
 		if (text[cursor] != '\0') {
 			cursor = strlen(text);
@@ -512,6 +517,7 @@ insert:
 	case XK_Escape:
 		cleanup();
 		exit(1);
+	case XK_Home:
 	case XK_KP_Home:
 		if (sel == matches) {
 			cursor = 0;
@@ -520,6 +526,7 @@ insert:
 		sel = curr = matches;
 		calcoffsets();
 		break;
+	case XK_Left:
 	case XK_KP_Left:
 		if (cursor > 0 && (!sel || !sel->left || lines > 0)) {
 			cursor = nextrune(-1);
@@ -528,18 +535,21 @@ insert:
 		if (lines > 0)
 			return;
 		/* fallthrough */
+	case XK_Up:
 	case XK_KP_Up:
 		if (sel && sel->left && (sel = sel->left)->right == curr) {
 			curr = prev;
 			calcoffsets();
 		}
 		break;
+	case XK_Next:
 	case XK_KP_Next:
 		if (!next)
 			return;
 		sel = curr = next;
 		calcoffsets();
 		break;
+	case XK_Prior:
 	case XK_KP_Prior:
 		if (!prev)
 			return;
@@ -556,6 +566,7 @@ insert:
 		if (sel)
 			sel->out = 1;
 		break;
+	case XK_Right:
 	case XK_KP_Right:
 		if (text[cursor] != '\0') {
 			cursor = nextrune(+1);
@@ -564,6 +575,7 @@ insert:
 		if (lines > 0)
 			return;
 		/* fallthrough */
+	case XK_Down:
 	case XK_KP_Down:
 		if (sel && sel->right && (sel = sel->right) == next) {
 			curr = next;
